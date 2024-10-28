9c63b7155e2dd7b6c467143445f2c8c595c63e1a
ImMaxence <maxence.bonnici2002@gmail.com>
2024-10-28 15:33:28 +0100

    init front

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -3,27 +3,27 @@ module.exports = (err, req, res, next) => {
 
     if (err.status === 401) {
         return res.status(401).json({
-            error: 'Unauthorized' + (err.message ? ': ' + err.message : '')
+            error: '❌ Unauthorized' + (err.message ? ': ' + err.message : '')
         });
     }
 
     if (err.status === 403) {
         return res.status(401).json({
-            error: 'Forbidden' + (err.message ? ': ' + err.message : '')
+            error: '❌ Forbidden' + (err.message ? ': ' + err.message : '')
         });
     }
 
     if (err.status === 404) {
         return res.status(401).json({
-            error: 'Not Found' + (err.message ? ': ' + err.message : '')
+            error: '❌ Not Found' + (err.message ? ': ' + err.message : '')
         });
     }
 
     if (err.status === 409) {
         return res.status(401).json({
-            error: 'Conflict' + (err.message ? ': ' + err.message : '')
+            error: '❌ Conflict' + (err.message ? ': ' + err.message : '')
         });
     }
 
-    res.status(500).json({ error: 'Something went wrong!' });
+    res.status(500).json({ error: '❌ Something went wrong!' });
 };
